f049d5911085ebd43d1522d7e8f0bfa4531c0a78
teddy19930521@gmail.com <fq39542099>
2018-03-05 01:20:01 +0800

    创建user包，配置依赖关系

--- APP/src/main/java/com/zhangflg/mykotlinmall/ui/activity/MainActivity.kt
+++ APP/src/main/java/com/zhangflg/mykotlinmall/ui/activity/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.zhangflg.mykotlinmall
+package com.zhangflg.mykotlinmall.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.zhangflg.mykotlinmall.R
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
