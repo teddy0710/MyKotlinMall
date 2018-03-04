0aab995d1d84d760899f424b0354fa2f81b63d36
teddy19930521@gmail.com <fq39542099>
2018-03-05 02:27:29 +0800

    kotlin视图插件extensions

--- UserCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
@@ -2,12 +2,15 @@ package com.kotlin.user.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.kotlin.user.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        mRegisterBtn.setOnClickListener({ Toast.makeText(this, "www", Toast.LENGTH_LONG).show() })
     }
 }
