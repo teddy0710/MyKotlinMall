7d547d3bfe3f9a0696988033bec4cf0c7a3afac8
teddy19930521@gmail.com <fq39542099>
2018-03-05 03:14:02 +0800

    anko

--- UserCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
@@ -2,8 +2,8 @@ package com.kotlin.user.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.kotlin.user.R
+import org.jetbrains.anko.toast
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -11,6 +11,9 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        mRegisterBtn.setOnClickListener({ Toast.makeText(this, "www", Toast.LENGTH_LONG).show() })
+        mRegisterBtn.setOnClickListener {
+            toast("注册")
+        }
+
     }
 }
