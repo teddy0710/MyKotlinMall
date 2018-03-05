414375aa5c10a72a5ca165f9456be81b6019f55b
zhangflg <password123>
2018-03-05 13:59:38 +0800

    封装rxjava 的execute（observeOn，subscribeOn，subscribe）

---
+++ BaseLibrary/src/main/java/com/kotlin/base/ext/CommonExt.kt
@@ -0,0 +1,15 @@
+package com.kotlin.base.ext
+
+import com.kotlin.base.rx.BaseSubscriber
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+
+/**
+ * Created by ZFL on 2018/3/5
+ */
+fun <T> Observable<T>.execute(subscriber: BaseSubscriber<T>) {
+    this.observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe(subscriber)
+}
--- UserCenter/src/main/java/com/kotlin/user/ui/presenter/view/RegisterPresenter.kt
+++ UserCenter/src/main/java/com/kotlin/user/ui/presenter/view/RegisterPresenter.kt
@@ -1,11 +1,9 @@
 package com.kotlin.user.ui.presenter.view
 
+import com.kotlin.base.ext.execute
 import com.kotlin.base.presenter.BasePresenter
 import com.kotlin.base.rx.BaseSubscriber
 import com.kotlin.user.service.impl.UserServiceImpl
-import rx.Subscriber
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
 
 /**
  * Created by ZFL on 2018/3/5
@@ -17,9 +15,7 @@ class RegisterPresenter : BasePresenter<RegisterView>() {
          */
         val userService = UserServiceImpl()
         userService.register(mobile, verifyCode, pwd)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe(object : BaseSubscriber<Boolean>() {
+                .execute(object : BaseSubscriber<Boolean>() {
                     override fun onNext(t: Boolean) {
                         mView.onRegisterResult(t)
                     }
