40ed8ab8600885dabda3df9a37e8b19c5da01dcd
teddy19930521@gmail.com <fq39542099>
2018-03-12 02:33:41 +0800

    商品一级二级布局

---
+++ GoodsCenter/src/main/java/com/kotlin/goods/ui/adapter/TopCategoryAdapter.kt
@@ -0,0 +1,33 @@
+package com.kotlin.goods.ui.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kotlin.base.ui.adapter.BaseRecyclerViewAdapter
+import com.kotlin.goods.R
+import com.kotlin.goods.data.protocol.Category
+import kotlinx.android.synthetic.main.layout_top_category_item.view.*
+
+/**
+ * Created by ZFL on 2018/3/10
+ */
+class TopCategoryAdapter(context: Context) : BaseRecyclerViewAdapter<Category, TopCategoryAdapter.ViewHolder>(context) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(mContext)
+                .inflate(R.layout.layout_top_category_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        super.onBindViewHolder(holder, position)
+        val modle = dataList[position]
+        holder.itemView.mTopCategoryNameTv.text = modle.categoryName
+        holder.itemView.mTopCategoryNameTv.isSelected = modle.isSelected
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+}
